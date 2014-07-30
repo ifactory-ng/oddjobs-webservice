db1f80640ce9b58305a4dfd01ce9f965da628940
smile <mumene@gmail.com>
2014-07-30 14:38:58 +0100

    init

--- conf/auth.js
+++ conf/auth.js
@@ -1,7 +1,7 @@
 var ids = {
 facebook: {
- clientID: 'empty',
- clientSecret: 'empty',
+ clientID: '747991285264118',
+ clientSecret: '6b60a8afffd172af54aedf3904a5d28b',
  callbackURL: 'http://127.0.0.1:1337/auth/facebook/callback'
 },
 twitter: {
