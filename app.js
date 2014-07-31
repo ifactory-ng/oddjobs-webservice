cb8fcebf345f1a613e23f520835902dff0f45beb
smile <mumene@gmail.com>
2014-07-31 11:25:48 +0100

    init

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ passport.deserializeUser(function(obj, done) {
 done(null, obj);
 });
 var User = schema.userModel;
-var user = '';
+
 // config
 passport.use(new FacebookStrategy({
 clientID: config.facebook.clientID,
@@ -80,31 +80,25 @@ function(req, res){
 
 //profile section
 app.get('/profile/:id?', ensureAuthenticated, function(req, res){
-  
-/*  if(req.session.passport.user){
-user = req.session.passport.user;
-  }
-  else{
-    user = req.user.id
-  }
-  */
-  User.findById(user, function(err, user){
+	User.findById(req.session.passport.user, function(err, user){
 		if(err) {
 			console.log(err);
-			res.send(500);
+			res.json(401);
 		}
+		else{
 			res.json(200, user);
+		}
 	});
 });
 
-//app.get('/profile/:id', api.user);
+app.get('/profile/:id', api.user);
 app.put('/profile/update/about', api.about);
 app.post('/profile/create/shop/:id', api.createShop);
 app.post('/shop/addProduct/');
 
 function ensureAuthenticated(req, res, next){
 	if (req.isAuthenticated()) { return next(); }
-	res.send(500);
+	res.send(404);
 }
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
