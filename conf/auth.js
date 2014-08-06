8ba90b9a99771d8d17b05b326112cf7314aa7eb4
smile <mumene@gmail.com>
2014-08-06 11:48:54 +0100

    corrections

--- conf/auth.js
+++ conf/auth.js
@@ -2,7 +2,7 @@ var ids = {
 facebook: {
  clientID: '747991285264118',
  clientSecret: '6b60a8afffd172af54aedf3904a5d28b',
- callbackURL: 'http://oddjobs.herokuapp.com/auth/facebook/callback'
+ callbackURL: 'oddjobs.herokuapp.com/auth/facebook/callback'
 },
 twitter: {
  consumerKey: '',
