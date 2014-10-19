f66bb93b7fdcfbcf47ae43e06709d46acd863412
smile <mumene@gmail.com>
2014-10-19 22:46:47 +0100

    edit

--- routes/users.js
+++ routes/users.js
@@ -37,6 +37,7 @@ router.param('user_id', function(req, res, next, id){
 
 router.put('/profile/update/about/:user_id?',   function(req, res, next){
 	var obj = req.body.about;
+	console.log(req.body);
 console.log("the" + obj);
 console.log(req.user);
 	user.findByIdAndUpdate(req.user, {
