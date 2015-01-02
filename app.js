1ba5278aaf6bf208858c4e4245cb36502ad0b670
smile <mumene@gmail.com>
2015-01-02 16:34:11 +0100

    access control

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', '*');
-
+				 res.setHeader('Access-Control-Allow-Credentials', true);
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         next();
